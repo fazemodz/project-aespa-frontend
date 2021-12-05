c9b26c0eda5a8043a57743c3faa752209a7876b9
fazemodz <45793524+fazemodz@users.noreply.github.com>
2021-12-05 12:54:17 +0000

    random tidy up

--- src/modules/headercomponents/VideoBackground.js
+++ src/modules/headercomponents/VideoBackground.js
@@ -1,62 +1,58 @@
-import React from 'react'
+import React from "react";
 import ReactDom from "react-dom";
 import ReactPlayer from "react-player";
 import AespaVideo from "../../Resources/Videos/aespasavage.mp4";
-import '../../Resources/css/BackgroundVideo.css'
-import 'animate.css';
+import "../../Resources/css/BackgroundVideo.css";
+import "animate.css";
 export default class VideoBackground extends React.Component {
   state = {
     HasVideoRenderFailed: false,
-  }
+  };
   RenderVideoFrame() {
     return (
       <div>
         {/* creates a react player from file */}
         <ReactPlayer
-    // animate__animated animate__fadeInLeft are used to animate the video background
-      className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
-      loop
-      muted
-      playing
-      controls={false}
-      url={AespaVideo}
-      onError={() => this.setState({ HasVideoRenderFailed: true })}
-    />
+          // animate__animated animate__fadeInLeft are used to animate the video background
+          className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
+          loop
+          muted
+          playing
+          controls={false}
+          url={AespaVideo}
+          onError={() => this.setState({ HasVideoRenderFailed: true })}
+        />
       </div>
-    )
+    );
   }
-  RenderVideoFrameFallBack(){
-      console.log("Video loading faile falling to Render Video Frame FallBack");
-      // fallback to stream video url if load from file fails due to Git LFS
-      return (
-        <div>
-          <ReactPlayer
-    // animate__animated animate__fadeInLeft are used to animate the video background
-      className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
-      loop
-      muted
-      playing
-      controls={false}
-      url="https://streamable.com/x5g3s7"
-    />
-        </div>
-      )
+  RenderVideoFrameFallBack() {
+    console.log("Video loading faile falling to Render Video Frame FallBack");
+    // fallback to stream video url if load from file fails due to Git LFS
+    return (
+      <div>
+        <ReactPlayer
+          // animate__animated animate__fadeInLeft are used to animate the video background
+          className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
+          loop
+          muted
+          playing
+          controls={false}
+          url="https://streamable.com/x5g3s7"
+          height="100wh"
+          width="100vw"
+        />
+      </div>
+    );
   }
   render() {
     // Renders this function outside of the react app render target using ReactDom.createPortal()
     return ReactDom.createPortal(
-        <div>
-        {
-        this.state.HasVideoRenderFailed === false ? 
-        this.RenderVideoFrame() : 
-        this.RenderVideoFrameFallBack()
-        }
-       
+      <div>
+        {this.state.HasVideoRenderFailed === false
+          ? this.RenderVideoFrame()
+          : this.RenderVideoFrameFallBack()}
       </div>,
-      document.getElementById("video"),
-    )
+      document.getElementById("video")
+    );
   }
-  
-
 }
-
