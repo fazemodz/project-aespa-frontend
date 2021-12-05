677607993a54a4450d1dcaee09ceabf0aee7ef94
fazemodz <45793524+fazemodz@users.noreply.github.com>
2021-12-05 11:56:07 +0000

    Update VideoBackground.js

--- src/modules/headercomponents/VideoBackground.js
+++ src/modules/headercomponents/VideoBackground.js
@@ -4,27 +4,59 @@ import ReactPlayer from "react-player";
 import AespaVideo from "../../Resources/Videos/aespasavage.mp4";
 import '../../Resources/css/BackgroundVideo.css'
 import 'animate.css';
-export default function VideoBackground() {
-  let VideoUrl = AespaVideo;
-  // Renders this function outside of the react app render target using ReactDom.createPortal()
+export default class VideoBackground extends React.Component {
+  state = {
+    HasVideoRenderFailed: false,
+  }
+  RenderVideoFrame() {
+    return (
+      <div>
+        {/* creates a react player from file */}
+        <ReactPlayer
+    // animate__animated animate__fadeInLeft are used to animate the video background
+      className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
+      loop
+      muted
+      playing
+      controls={false}
+      url={AespaVideo}
+      onError={() => this.setState({ HasVideoRenderFailed: true })}
+    />
+      </div>
+    )
+  }
+  RenderVideoFrameFallBack(){
+      console.log("Video loading faile falling to Render Video Frame FallBack");
+      // fallback to stream video url if load from file fails due to Git LFS
+      return (
+        <div>
+          <ReactPlayer
+    // animate__animated animate__fadeInLeft are used to animate the video background
+      className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
+      loop
+      muted
+      playing
+      controls={false}
+      url="https://streamable.com/x5g3s7"
+    />
+        </div>
+      )
+  }
+  render() {
+    // Renders this function outside of the react app render target using ReactDom.createPortal()
     return ReactDom.createPortal(
         <div>
-        {/* Create a player for video background */}
-        <ReactPlayer
-        // animate__animated animate__fadeInLeft are used to animate the video background
-          className="Background_Vid_Aespa animate__animated animate__fadeInLeft"
-          loop
-          muted
-          playing
-          controls={false}
-          url={[{ src: VideoUrl, type: "video/mp4" }]}
-          onError={VideoUrl = "https://streamable.com/x5g3s7"}
-        />
+        {
+        this.state.HasVideoRenderFailed === false ? 
+        this.RenderVideoFrame() : 
+        this.RenderVideoFrameFallBack()
+        }
+       
       </div>,
       document.getElementById("video"),
     )
+  }
+  
 
 }
-const RenderHeaderText = () =>{
- 
-}
+
