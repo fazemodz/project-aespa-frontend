764f91d96f53dc9a180fd88c12b9fc22033b28bf
fazemodz <45793524+fazemodz@users.noreply.github.com>
2021-12-06 10:05:12 +0000

    work on home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./Resources/css/Home_Page.css";
 // import navlinks from "./modules/navlinks";
 import Header from "./modules/header";
 import React, { Component } from 'react'
-
+import Homepagebody from "./modules/homepagebody";
 export default class App extends Component {
   render() {
     return (
       <div>
          <Header/>
+         <Homepagebody/>
       </div>
     )
   }
