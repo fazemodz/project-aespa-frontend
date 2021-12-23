63cc950767f725fcc637b896fba5b8a0ad40b33a
fazemodz <45793524+fazemodz@users.noreply.github.com>
2021-12-23 15:12:42 +0000

    changed how background was rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./Resources/css/Home_Page.css";
-// import navlinks from "./modules/navlinks";
 import Header from "./modules/header";
 import React, { Component } from 'react'
-import Homepagebody from "./modules/homepagebody";
 export default class App extends Component {
   render() {
     return (
       <div>
          <Header/>
-         <Homepagebody/>
       </div>
     )
   }
