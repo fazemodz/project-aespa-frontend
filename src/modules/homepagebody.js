d9b53522e24402d349158d7bcd13ce1ad8096033
fazemodz <45793524+fazemodz@users.noreply.github.com>
2022-01-13 18:26:56 +0000

    fixed a render bug

--- src/modules/homepagebody.js
+++ src/modules/homepagebody.js
@@ -4,7 +4,6 @@ export default function Homepagebody() {
     return (
         <div>
             <div className="homepagebody">
-                <div className="Kwangyapicrendertarget"/>
                     <div className="KwangyaTextcontainer">
                         <p className="KawngiaMainText">
                             Step into the world of Kwangya 
