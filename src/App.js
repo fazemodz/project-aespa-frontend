826de2749b454409805c71953e7cd955d3eb275b
fazemodz <45793524+fazemodz@users.noreply.github.com>
2022-01-13 18:17:26 +0000

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./Resources/css/Home_Page.css";
 import Header from "./modules/header";
 import React, { Component } from 'react'
+import Homepagebody from './modules/homepagebody'
 export default class App extends Component {
   render() {
     return (
       <div>
          <Header/>
+         <Homepagebody/>
       </div>
     )
   }
